f0a8c9bc54f160a8df17dcae89f290c486d29204
Jaden Zaleski <jadenzaleski@icloud.com>
2024-02-18 19:32:39 -0500

    Chart at acceptable stage. build succeeded.

--- SwiftLift/Charts/VolumeVsDate.swift
+++ SwiftLift/Charts/VolumeVsDate.swift
@@ -9,59 +9,177 @@ import SwiftUI
 import SwiftData
 import Charts
 
+struct PlottingData: Identifiable {
+    var id = UUID()
+    var y: Double
+    var date: Date
+    var x: Int
+    
+    init(id: UUID = UUID(), x: Int, y: Double, date: Date) {
+        self.id = id
+        self.y = y
+        self.date = date
+        self.x = x
+    }
+}
+
 struct VolumeVsDate: View {
     @Environment(\.modelContext) private var modelContext
     @Environment(\.colorScheme) var colorScheme
     @Query private var history: [History]
     private let gradient = LinearGradient(gradient: Gradient(colors: [Color.accentColor.opacity(0.4), Color.white.opacity(0)]), startPoint: .top, endPoint: .bottom)
-    private let dateFormatter: DateFormatter = {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "MM/dd"
-            return formatter
-        }()
+    @Binding var pastDays: Int
+    @Binding var yAxis: String
     
+    //    var body: some View {
+    ////        let h = history[0]
+    //        let h = History.sampleHistory
+    //        let n = pastDays == -1 ? h.workouts!.count : min(pastDays, h.workouts?.count ?? 0)
+    //
+    //        let dates = h.workouts?.prefix(n).compactMap { $0.startDate } ?? []
+    //
+    //        let startDate = dates.min() ?? Date()
+    //        let endDate = dates.max() ?? Date()
+    //        let totalY = h.workouts!.reduce(0, { $0 + $1.totalWeight })
+    //        let avgY =  h.workouts!.isEmpty ? 0.0 : totalY / Double(h.totalWorkouts)
+    //
+    //        Chart {
+    //            ForEach(h.workouts!) { workout in
+    //                LineMark(x: .value("Date", workout.startDate),
+    //                         y: .value("Volume", workout.totalWeight))
+    //                .foregroundStyle(Color.accentColor)
+    //                .symbol {
+    //                    Image(systemName: "circle.fill")
+    //                        .font(.system(size: 8))
+    //                }
+    //
+    //            }
+    //
+    //            ForEach(h.workouts!) { workout in
+    //                AreaMark(x: .value("Date", workout.startDate),
+    //                         y: .value("Volume", workout.totalWeight))
+    //            }
+    //            .foregroundStyle(gradient)
+    //
+    //            RuleMark(y: .value("Average", avgY))
+    //                            .foregroundStyle(Color.secondary)
+    //                            .lineStyle(StrokeStyle(lineWidth: 0.8, dash: [10]))
+    //                            .annotation(alignment: .topTrailing) {
+    //                                Text("Avg: " + "\(Int(avgY))")
+    //                                    .font(.subheadline).bold()
+    //                                    .padding(.trailing, 32)
+    //                                    .foregroundStyle(Color.secondary)
+    //                            }
+    //        }
+    //        .chartXAxis {
+    //            AxisMarks(format: .dateTime.month(.defaultDigits).day(), values: dates)
+    //        }
+    ////        .chartXScale(domain: (startDate - 1)...(endDate + 12))
+    //        .frame(height: 250)
+    ////        .aspectRatio(1, contentMode: .fit)
+    //    }
     
     var body: some View {
-        let h = history[0]
-        let dates = h.workouts!.map { $0.startDate }
-        var volumes = h.workouts!.map { $0.totalWeight }
- 
-        let startDate = dates.min() ?? Date()
-        let endDate = dates.max() ?? Date()
+        //        let h = history[0]
+        let h = History.sampleHistory
+        //        let dates = h.workouts!.map { $0.startDate }
+        //        let volumes = h.workouts!.map { $0.totalWeight }
+        let n = pastDays == -1 ? h.workouts!.count : min(pastDays, h.workouts?.count ?? 0)
+        let dates = h.workouts?.prefix(n).compactMap { $0.startDate } ?? []
+        let sortedWorkouts = h.workouts!.sorted { (workout1, workout2) in
+            return workout1.startDate < workout2.startDate
+        }
+        
+        var data: [PlottingData] = sortedWorkouts.enumerated().map { index, workout in
+            return PlottingData(x: Int(index), y: workout.totalWeight, date: workout.startDate)
+        }
+        
+        
+        
+        //        var currentDay = Date.now
+        //        let n = min(pastDays, sortedWorkouts.count)
+        //        let data: [PlottingData] = (0..<n).map { i in
+        //            if (currentDay.distance(to: sortedWorkouts[i].startDate) > 24 * 60 * 60) {
+        //                currentDay = sortedWorkouts[i].startDate
+        //                return PlottingData(y: 0, date: sortedWorkouts[i].startDate);
+        //            } else {
+        //                currentDay = sortedWorkouts[i].startDate
+        //                return PlottingData(y: sortedWorkouts[i].totalWeight, date:
+        //                                        sortedWorkouts[i].startDate)
+        //            }
+        //
+        //        }
+        
+        
+        let maxX = data.max(by: { $0.x < $1.x })?.x ?? 0
+        let maxY = data.max(by: { $0.y < $1.y })?.y ?? 0
+        let totalY = data.reduce(0.0) { $0 + $1.y }
+        let avgY =  data.isEmpty ? 0.0 : totalY / Double(data.count)
+        let maxDate = data.map { $0.date }.max() ?? Date()
+        let minDate = data.map { $0.date }.min() ?? Date()
+        let dateFormatter = DateFormatter()
+        
+        Text("\(n)");
+        Text("\(yAxis)");
         
         Chart {
-            ForEach(h.workouts!) { workout in
-                let formattedDate = dateFormatter.string(from: workout.startDate)
-                LineMark(x: .value("Date", workout.startDate),
-                         y: .value("Volume", workout.totalWeight))
+            ForEach(data) { d in
+                LineMark(
+                    x: .value("Day", d.x),
+                    y: .value("Weight", d.y)
+                )
                 .foregroundStyle(Color.accentColor)
-
-                PointMark(x: .value("Date", workout.startDate),
-                          y: .value("Volume", workout.totalWeight))
+            }
+            .symbol {
+                Circle()
+                    .fill(Color.accentColor)
+                    .frame(width: 8)
+            }
+            ForEach(data) { d in
+                AreaMark(
+                    x: .value("Day", d.x),
+                    yStart: .value("low", 0),
+                    yEnd: .value("high",  d.y)
+                )
+                .foregroundStyle(gradient)
                 
             }
-            .interpolationMethod(.cardinal)
+            RuleMark(y: .value("Average", avgY))
+                .foregroundStyle(Color.secondary)
+                .lineStyle(StrokeStyle(lineWidth: 0.8, dash: [10]))
+                .annotation(alignment: .topTrailing) {
+                    Text("Avg: " + "\(Int(avgY))")
+                        .font(.subheadline).bold()
+                        .padding(.trailing, 32)
+                        .foregroundStyle(Color.secondary)
+                }
             
-            ForEach(h.workouts!) { workout in
-                AreaMark(x: .value("Date", workout.startDate),
-                         y: .value("Volume", workout.totalWeight))
-            }
-            .interpolationMethod(.cardinal)
-            .foregroundStyle(gradient)
         }
+        .frame(height: 250)
+        
         .chartXAxis {
-            AxisMarks(format: .dateTime.month(.defaultDigits).day(), values: dates)
+            AxisMarks(preset: .aligned, values: data.map { $0.x }) { value in
+                if let index = value.as(Int.self) {
+                    AxisGridLine()
+                    AxisValueLabel(orientation: .vertical, horizontalSpacing: -6.5, verticalSpacing: 6.5) {
+                        if let pd = data.first(where: { $0.x == index }) {
+                            Text("\(pd.date, format: .dateTime.month(.twoDigits).day(.twoDigits))")
+                                
+                        }
+                    }
+                    
+                }
+            }
+            
         }
-//        .chartYAxis {
-//            
-//            AxisMarks(values: volumes)
-//        }
-        .chartXScale(domain: (startDate - 1)...(endDate + 12))
-        .aspectRatio(1, contentMode: .fit)
+        .chartYScale(domain: 0...(maxY + 100))
+        //        .chartScrollableAxes(.horizontal)
+        //        .chartScrollPosition(initialX: minDate.timeIntervalSince1970)
     }
 }
 
+
 #Preview {
-    VolumeVsDate()
+    VolumeVsDate(pastDays: .constant(30), yAxis: .constant("volume"))
         .modelContainer(previewContainer)
 }
--- SwiftLift/Views/StatsView.swift
+++ SwiftLift/Views/StatsView.swift
@@ -8,6 +8,26 @@
 import SwiftUI
 
 struct StatsView: View {
+    enum DateYValue: String, CaseIterable, Identifiable {
+        case volume, reps, duration
+        var id: Self { self }
+        var text: String {
+            get {
+                switch self {
+                case .duration: return "duration"
+                case .reps: return "reps"
+                case .volume: return "volume"
+                }
+            }
+            nonmutating set {
+                // do nothing when set becuase we only read this value
+            }
+        }
+    }
+    @State private var selectedYDateValue: DateYValue = .volume
+
+    @State private var selectedPastLength: Int = 7
+    
     var body: some View {
         /*
          - total workouts
@@ -19,12 +39,13 @@ struct StatsView: View {
          - reps of day x date
          - length of workouts x date
          - select a workout
-            - weight x set
-            - reps x set
-            - more data about workout
+         - weight x set
+         - reps x set
+         - more data about workout
          -
          */
         ScrollView {
+            
             VStack(spacing: 20) {
                 HStack {
                     Text("Stats View")
@@ -32,24 +53,60 @@ struct StatsView: View {
                     Spacer()
                 }
                 VStack {
-                    Text("VolumeVsDate")
-                    VolumeVsDate()
+                    
                 }
                 .padding()
                 .background()
                 .clipShape(RoundedRectangle(cornerRadius: 15))
                 
                 VStack {
+                    Picker("Y Value", selection: $selectedYDateValue) {
+                        Text("Volume").tag(DateYValue.volume)
+                        Text("Reps").tag(DateYValue.reps)
+                        Text("Duration").tag(DateYValue.duration)
+                    }
+                    .pickerStyle(.segmented)
+                    Picker("Y Value", selection: $selectedPastLength) {
+                        Text("7").tag(7)
+                        Text("30").tag(30)
+                        Text("90").tag(90)
+                        Text("180").tag(180)
+                        Text("365").tag(365)
+                        Text("All").tag(-1)
+                    }
+                    .padding(.top, 8.0)
+                    .pickerStyle(.segmented)
                     
+                    HStack {
+                        Text("\(selectedYDateValue.text.capitalized) vs. Date")
+                            .font(.title3)
+                            .bold()
+                        Spacer()
+                    }
+                    .padding(.top, 20.0)
+                    HStack {
+                        if (selectedPastLength == -1) {
+                            Text("All workouts")
+                                .font(.subheadline)
+                                .foregroundStyle(.secondary)
+                        } else {
+                            Text("Past \(selectedPastLength) workouts")
+                                .font(.subheadline)
+                                .foregroundStyle(.secondary)
+                        }
+                        Spacer()
+                    }
+                    VolumeVsDate(pastDays: $selectedPastLength, yAxis: $selectedYDateValue.text)
                 }
                 .padding()
                 .background()
                 .clipShape(RoundedRectangle(cornerRadius: 15))
+                
             }
             .padding()
         }
         .background(Color("offset"))
-
+        
     }
 }
 
