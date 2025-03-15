955fa44ceeef6a739cd703fae56c3fd75752c061
Jaden Zaleski <jadenzaleski@icloud.com>
2025-03-14 22:55:42 -0400

    Working on adding tests

---
+++ SwiftLiftUnitTests/SwiftLiftUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftLiftUnitTests.swift
+//  SwiftLiftUnitTests
+//
+//  Created by Jaden Zaleski on 3/14/25.
+//
+
+import Testing
+
+struct SwiftLiftUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
