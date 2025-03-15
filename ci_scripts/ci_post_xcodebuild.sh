1f815021c59bbfe05bef6a0bbe70a3529b38aa75
Jaden Zaleski <jadenzaleski@icloud.com>
2025-03-14 21:33:00 -0400

    ci_post_xcodebuild.sh updates.

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -10,7 +10,7 @@ if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
     TESTFLIGHT_DIR_PATH=../TestFlight
     mkdir $TESTFLIGHT_DIR_PATH
      # Fetch the latest merge commit message and append it to the build notes
-    merge_commit_message=$(git log --merges -1 --pretty=format:"%s")
+    merge_commit_message=$(git fetch --deepen 1 && git log -1 --pretty=format:"%s")
     echo "Merge commit message: $merge_commit_message"
     echo -e "$merge_commit_message" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
