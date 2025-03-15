687190776d754bb206350067d25f361b2a9eddb3
Jaden Zaleski <jadenzaleski@icloud.com>
2025-03-14 21:17:49 -0400

    Updated CI again.

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -6,13 +6,11 @@
 #  Created by Jaden Zaleski on 3/13/25.
 #
 
-echo "Hello World from ci_scripts/ci_post_xcodebuild.sh"
-
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
-    echo "in the if loop"
     TESTFLIGHT_DIR_PATH=../TestFlight
     mkdir $TESTFLIGHT_DIR_PATH
      # Fetch the latest merge commit message and append it to the build notes
     merge_commit_message=$(git log --merges -1 --pretty=format:"%s")
+    echo "Merge commit message: $merge_commit_message"
     echo -e "$merge_commit_message" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
